56159450905d5c622f585294265259b1b42eb2f6
MH.Yazdani <mohaymen@YAZDANI-PC>
2026-08-18 17:21:30 +0330

    adding some tests

---
+++ Bookmark.Test/Bookmarks/BookmarksUnitTests.cs
@@ -0,0 +1,52 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bookmark.Test.Bookmarks
+{
+    public class BookmarksUnitTests
+    {
+        [Fact]
+        public void CreateBookmark_WithValidData_ReturnsCreatedBookmark()
+        {
+
+        }
+        [Fact]
+        public void CreateBookmark_WithEmptyTitle_ThrowsValidationException()
+        {
+
+        }
+        [Fact]
+        public void CreateBookmark_WithInvalidUrl_ThrowsValidationException()
+        {
+
+        }
+        [Fact]
+        public void CreateBookmark_WithNewTagNames_CreatesTagsThenLinksThem()
+        {
+
+        }
+        [Fact]
+        public void CreateBookmark_WithExistingTagNames_ReusesExistingTags()
+        {
+
+        }
+        [Fact]
+        public void CreateBookmark_OnSuccess_InvalidatesListCache()
+        {
+
+        }
+        [Fact]
+        public void ReadBookmark_OnSuccess_ValidId()
+        {
+
+        }
+
+        [Fact]
+        public void ReadBookmark_WithInvalidId_ReturnNull()
+        {
+
+        }
+    }
+}
