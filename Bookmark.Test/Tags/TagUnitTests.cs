8b8ceae7e79111fe993ab6396df3dcede667def8
MH.Yazdani <mohaymen@YAZDANI-PC>
2026-08-23 15:37:28 +0330

    adding tag unit tests into project.

--- Bookmark.Test/Tags/TagUnitTests.cs
+++ Bookmark.Test/Tags/TagUnitTests.cs
@@ -6,5 +6,40 @@ namespace Bookmark.Test.Tags
 {
     public class TagUnitTests
     {
+        [Fact]
+        public void ListTags_CacheHit_DoesNotCallRepository() 
+        { 
+        
+        }
+        [Fact]
+        public void ListTags_CacheMiss_FallsBackToRepository() 
+        { 
+        
+        }
+        [Fact]
+        public void ListTags_CacheMiss_PopulatesCache() 
+        { 
+        
+        }
+        [Fact]
+        public void ListTags_ReturnsTagsWithBookmarkCount() 
+        { 
+        
+        }
+        [Fact]
+        public void CreateTag_WithDuplicateName_ThrowsConflictException() 
+        { 
+        
+        }
+        [Fact]
+        public void DeleteTag_Orphaned_DeletesSuccessfully() 
+        { 
+        
+        }
+        [Fact]
+        public void DeleteTag_InvalidatesTagsCache() 
+        { 
+        
+        }
     }
 }
