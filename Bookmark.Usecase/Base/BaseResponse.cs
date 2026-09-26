4d5cac4b0f82292350bdb8020d5a0a7990375c1b
Mohammadhasan <mryazdani98@outlook.com>
2026-09-26 07:22:10 +0330

    update base response

--- Bookmark.Usecase/Base/BaseResponse.cs
+++ Bookmark.Usecase/Base/BaseResponse.cs
@@ -2,11 +2,29 @@
 {
     public class BaseResponse
     {
-       public bool IsSuccess { get; set; }
+        protected internal BaseResponse(bool isSuccess, string? message)
+        {
+            IsSuccess = isSuccess;
+            Message = message;
+        }
+        private bool IsSuccess { get; set; }
+        private string? Message { get; set; }
+
+        public static BaseResponse success() => new (true, null);
+        public static BaseResponse failure(string message) => new (false, message);
+        public static BaseResponse<T>(T data)
+
     }
 
     public class BaseResponse<T> : BaseResponse
     {
+        public T Data { get; set; }
+
+        protected internal BaseResponse(T data, bool isSuccess, string? message): base(isSuccess, message)
+        {
+            Data = data;
+        }
+
 
     }
 }
